43d2b140a2f99f9e3daed8c2f14d4ab2c231776a
rinalfauzi <rinalfauzi@gmail.com>
2020-12-23 11:43:03 +0700

    First push

---
+++ src/main/java/com/example/demo/controller/HomeResource.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+    @GetMapping("/")
+    public String home(){
+        return("<h1>Welcome Home</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return("<h1>Welcome Admin</h1>");
+    }
+
+    @RequestMapping("/403")
+    public String accessDenied() {
+        return "403 error";
+    }
+}
